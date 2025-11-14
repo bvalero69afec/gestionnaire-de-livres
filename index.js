d8a40cd6e3ab1b7036d8fb9bdf90a8c09f50845e
Benjamin Valero <bvalero69afec@proton.me>
2025-11-14 11:32:09 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const addBookForm = document.querySelector("#add-book-form");
+const bookTitleInput = document.querySelector("#book-title-input");
+const bookAuthorInput = document.querySelector("#book-author-input");
+const bookCategorySelect = document.querySelector("#book-category-select");
+const noBooksText = document.querySelector("#no-books-text");
+const myBooksList = document.querySelector("#my-books-list");
+
+
+addBookForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const myBookItem = document.createElement("li");
+  myBookItem.classList.add("my-book-item");
+  myBooksList.appendChild(myBookItem);
+
+  const bookTitle = bookTitleInput.value;
+  const myBookTitle = document.createElement("h4");
+  myBookTitle.classList.add("my-book-title");
+  myBookTitle.textContent = `📚 ${bookTitle}`;
+  myBookItem.appendChild(myBookTitle);
+  bookTitleInput.value = "";
+
+  const bookAuthor = bookAuthorInput.value;
+  const myBookAuthor = document.createElement("span");
+  myBookAuthor.classList.add("my-book-author");
+  myBookAuthor.textContent = bookAuthor;
+  myBookItem.appendChild(myBookAuthor);
+  bookAuthorInput.value = "";
+
+  const bookCategory = bookCategorySelect.value;
+  const myBookCategory = document.createElement("span");
+  myBookCategory.classList.add("my-book-category");
+  myBookCategory.textContent = bookCategory;
+  myBookItem.appendChild(myBookCategory);
+  bookCategorySelect.value = "";
+
+  noBooksText.classList.add("hide");
+  myBooksList.classList.remove("hide");
+});
